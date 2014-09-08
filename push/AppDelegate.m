3ab58114e007e3cd01fd36d0fbe3bd83e1b9328d
Suo Qin <kpsesuo@gmail.com>
2014-09-08 10:52:35 +0800

    init push example repo

--- push/AppDelegate.m
+++ push/AppDelegate.m
@@ -12,6 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [application registerForRemoteNotificationTypes:(UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound)];
     // Override point for customization after application launch.
     return YES;
 }
@@ -41,6 +42,15 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    NSLog(@"my token is <%@>", deviceToken);
+
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    NSLog(@"push registration fail, the error is <%@>", error.localizedDescription);
 }
 
 @end
